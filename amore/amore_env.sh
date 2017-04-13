f83267c83c174a9a0ba28c7f2cc67f5b510755ba
laphecet <laurent.aphecetche@cern.ch>
2017-04-13 17:41:06 +0200

    Trying to use socat to link deroot pipes between containers

---
+++ amore/amore_env.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# amore setup within a container
+
+export DATE_SITE=/dateSite
+. /date/setup.sh
+
+# AMORE and DIM
+export AMORE=${AMORE:=/opt/amore}
+export AMORE_SITE=${AMORE_SITE:=/amoreSite}
+export LD_LIBRARY_PATH=${AMORE_SITE}/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=${AMORE}/lib:$LD_LIBRARY_PATH
+export PATH=${AMORE}/bin:$PATH
+export LD_LIBRARY_PATH=/opt/dim/linux:$LD_LIBRARY_PATH
+# AMORE extras
+export AMORE_CDB_URI=${AMORE_CDB_URI:=local:///local/cdb}
+# ROOT
+export ROOTSYS=${ROOTSYS:=/opt/root}
+export PATH=$ROOTSYS/bin:$PATH
+export LD_LIBRARY_PATH=$ROOTSYS/lib:$LD_LIBRARY_PATH
+
+. amoreSetup $AMORE_SITE/AMORE.params
+
+if [ -z ${ALICE_ROOT+x} ]; then
+    if test -d /opt/aliroot; then
+        export ALICE_ROOT=/opt/aliroot
+        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ALICE_ROOT/lib
+        export PATH=$PATH:/opt/aliroot/bin
+    fi
+else
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ALICE_ROOT/lib
+fi
+
+
